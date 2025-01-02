25e3969aa35403278044bb562d6583bcbcc553fb
khalilDataAnalyst <se3job@gmail.com>
2025-01-02 17:21:54 +0400

    SQL Queries

---
+++ HR Data Questions.sql
@@ -0,0 +1,13 @@
+ --- QUESTIONS
+ --- 1-whats is gender breakdwon of employees in the company?
+--- 2- whats is the race(Ethncity) breakdwon of employees in the  company?
+--- 3. what is the age distribution of employees in the company?
+--- 4-How many employees work at headquarters versus remote location?
+--- 5-what is the average length of employment for employees who has been terminated?
+--- 6-How does gender the Disteribution vary across departments and job title?
+--- 7- what is the distribution of jobtitles across the company?
+--- 8- Which department has highest turnover rate?
+--- 9- what is the distribution of employees across locations by state?
+--- 10- How has the companys employee changed over time based on hire date and term date?
+--- 11- what is the tenure distribution of each department?
+
